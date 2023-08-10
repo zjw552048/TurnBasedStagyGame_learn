d845e7d16adcea719c4805324c28e23e64aeb911
zjw-pc <>
2023-08-10 17:29:00 +0800

    追加控制camera的缩放控制（调整CinemachineTransposer的m_FollowOffset.y实现）

--- Assets/Scripts/VirtualCameraController.cs
+++ Assets/Scripts/VirtualCameraController.cs
@@ -1,12 +1,28 @@
+using System;
+using Cinemachine;
 using UnityEngine;
 
 public class VirtualCameraController : MonoBehaviour {
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = 100f;
+    [SerializeField] private float zoomSpeed = 5f;
+    [SerializeField] private float minFollowOffsetY = 2f;
+    [SerializeField] private float maxFollowOffsetY = 12f;
+
+    private CinemachineTransposer transposer;
+    private float targetCameraFollowOffsetY;
+
+    private void Awake() {
+        transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        targetCameraFollowOffsetY = transposer.m_FollowOffset.y;
+    }
+
 
     private void Update() {
         HandleMovement();
         HandleRotation();
+        HandleZoom();
     }
 
     private void HandleMovement() {
@@ -42,5 +58,21 @@ public class VirtualCameraController : MonoBehaviour {
         }
 
         transform.eulerAngles += rotateVector * rotateSpeed * Time.deltaTime;
+    }
+
+    private void HandleZoom() {
+        var followOffset = transposer.m_FollowOffset;
+        if (Input.mouseScrollDelta.y > 0) {
+            targetCameraFollowOffsetY += 1f;
+        }
+
+        if (Input.mouseScrollDelta.y < 0) {
+            targetCameraFollowOffsetY -= 1f;
+        }
+
+        targetCameraFollowOffsetY = Mathf.Clamp(targetCameraFollowOffsetY, minFollowOffsetY, maxFollowOffsetY);
+
+        followOffset.y = Mathf.SmoothStep(followOffset.y, targetCameraFollowOffsetY, zoomSpeed * Time.deltaTime);
+        transposer.m_FollowOffset = followOffset;
     }
 }
