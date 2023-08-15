18cf41e4d2f66c4559591146e55ac5372d85124f
zjw-pc <>
2023-08-15 11:11:41 +0800

    添加GrenadeAction空客

---
+++ Assets/Scripts/Actions/GrenadeAction.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeAction : BaseAction {
+    [SerializeField] private int maxThrowGrid = 9;
+    [SerializeField] private float rotateSpeed = 10f;
+
+    public override string GetActionName() {
+        return "Grenade";
+    }
+
+    private void Update() {
+        if (!actionActive) {
+            return;
+        }
+
+        // TODO
+        Debug.Log("Grenade Action!");
+        ActionComplete();
+    }
+
+    public override void TakeAction(GridPosition targetGridPosition, Action actionCompletedCallback) {
+        // TODO
+        ActionStart(actionCompletedCallback);
+    }
+
+    public override List<GridPosition> GetValidActionGridPositions() {
+        var baseGridPosition = unit.GetGridPosition();
+        var validGridPositionList = new List<GridPosition>();
+        for (var x = -maxThrowGrid; x <= maxThrowGrid; x++) {
+            for (var z = -maxThrowGrid; z <= maxThrowGrid; z++) {
+                var testGridPosition = baseGridPosition + new GridPosition(x, z);
+                if (!LevelGrid.Instance.IsValidGridPosition(testGridPosition)) {
+                    continue;
+                }
+
+                if (Mathf.Abs(x) + Mathf.Abs(z) > maxThrowGrid) {
+                    continue;
+                }
+
+                validGridPositionList.Add(testGridPosition);
+            }
+        }
+
+        return validGridPositionList;
+    }
+
+    public override List<EnemyAIAction> GetEnemyAIAction() {
+        // TODO
+        return new List<EnemyAIAction> {
+            new() {
+                gridPosition = unit.GetGridPosition(),
+                actionPriority = 0
+            }
+        };
+    }
+}
