14c2baab80547e4d9f0a97b334b51c3e552e3754
zjw-pc <>
2023-08-30 22:11:39 +0800

    保持屏幕常亮

--- Assets/Scripts/Manager/ResetStaticDataManager.cs
+++ Assets/Scripts/Manager/ResetStaticDataManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class ResetStaticDataManager : MonoBehaviour {
     private void Start() {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+            
         SwordAction.ResetStaticData();
         Unit.ResetStaticData();
         BulletProjectile.ResetStaticData();
